91946f9e45885a6bdbe7caa9c418b8ae83da4876
GCastro68 <gcastro2805@gmail.com>
2021-09-01 20:31:09 -0600

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'gusTavo caStro';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Gustavo',
+  };
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.info(this.valor);
+    console.info(this.obj);
+  }
 }
