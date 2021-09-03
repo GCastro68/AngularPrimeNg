8becc0465d6a19835f7379035903b39eaa8e447a
GCastro68 <gcastro2805@gmail.com>
2021-09-02 19:48:21 -0600

    Pipes Basicos

---
+++ src/app/ventas/pages/basicos/basicos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basicos',
+  templateUrl: './basicos.component.html',
+  styles: [],
+})
+export class BasicosComponent {
+  nombreLower: string = 'gustavo';
+  nombreUpper: string = 'GUSTAVO';
+  nombreCompleto: string = 'gUsTavo casTRo';
+}
