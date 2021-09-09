7dc2f0feac6da54978b267c614b87f9ce7d486b1
GCastro68 <gcastro2805@gmail.com>
2021-09-08 20:14:15 -0600

    Pipes personalizados

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,9 +3,22 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { MenubarModule } from 'primeng/menubar';
+
 import { FieldsetModule } from 'primeng/fieldset';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TableModule } from 'primeng/table';
+import { MenuItem } from 'primeng/api';
 
 @NgModule({
-  exports: [ButtonModule, CardModule, MenubarModule, FieldsetModule],
+  exports: [
+    ButtonModule,
+    CardModule,
+    MenubarModule,
+    FieldsetModule,
+    SplitButtonModule,
+    TableModule,
+    ToolbarModule,
+  ],
 })
 export class PrimeNgModule {}
--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import { rejects } from 'assert';
+// import { resolve } from 'dns';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -31,4 +34,36 @@ export class NoComunesComponent {
   borrarCliente() {
     this.clientes.shift();
   }
+
+  //KeyValue Pipe
+  persona = {
+    nombre: 'Gustavo',
+    edad: 35,
+    direccion: 'Ottawa, Canadá',
+  };
+
+  //Json Pipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false,
+    },
+  ];
+
+  //Async Pipe
+  miObservable = interval(1000); //0,1,2,3,4,5,6, ....
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 }
